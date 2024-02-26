387c7befbb9084ebe7fd8dc0c5c332dcbbfe9537
hdangar <hdangar@synoptek.com>
2024-02-26 10:55:05 +0530

    Register Method in AuthController

---
+++ JobHunt.Services.AuthAPI/Controllers/AuthController.cs
@@ -0,0 +1,64 @@
+﻿using JobHunt.Services.AuthAPI.Models;
+using JobHunt.Services.AuthAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobHunt.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AuthController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto request)
+        {
+            var user = new ApplicationUser
+            {
+                FullName = request.FirstName + " " + request.LastName,
+                UserName = request.Email?.Trim(),
+                Email = request.Email?.Trim(),
+                PhoneNumber = request.PhoneNumber?.Trim(),
+            };
+
+            var identityResult = await _userManager.CreateAsync(user, request.Password);
+            if (identityResult.Succeeded)
+            {
+                identityResult = await _userManager.AddToRoleAsync(user, request.Role);
+                if (identityResult.Succeeded)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    if (identityResult.Errors.Any())
+                    {
+                        foreach (var error in identityResult.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (identityResult.Errors.Any())
+                {
+                    foreach (var error in identityResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return ValidationProblem(ModelState);
+        }
+    }
+}
